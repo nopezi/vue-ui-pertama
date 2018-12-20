9230b3b90aafdeb08c8f1fdac2cfbf6dcfd9d8fc
nopezi saputra <snopezi@gmail.com>
2018-12-20 21:31:14 +0700

    tambah bootstrap

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
+import '@babel/polyfill'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false
 
